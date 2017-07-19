7bf6a813832e26054ac3e5fb8bf36d0f328e1361
Paul T <developer.paul.123@gmail.com>
2017-07-19 19:49:29 -0400

    Added number wizard project, now with a UI.

---
+++ NumberWizardUI/Assets/NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+    int guess;
+    int max;
+    int min;
+    int maxGuessesAllowed;
+
+    public Text NumberGuessText;
+
+	// Use this for initialization
+	void Start () {
+        StartGame();
+    }
+	
+    void StartGame()
+    {
+        
+        max = 1000;
+        min = 1;
+        maxGuessesAllowed = 5;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max+1);
+        NumberGuessText.text = guess.ToString();
+        maxGuessesAllowed--;
+        if (maxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+
+}
