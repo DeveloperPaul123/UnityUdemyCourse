006693ca7f2b1486dee45bf1060b9ae247a0f20c
DeveloperPaul123 <developer.paul.123@gmail.com>
2017-07-22 21:19:29 -0400

    Removed ignored files.

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        // take difference of coordinates of ball and paddle
+        paddleToBallVector = this.transform.position -
+            paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+        {
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            // launch the ball
+            GetComponent<Rigidbody2D>().velocity =
+                new Vector2(2f, 10f);
+        }
+	}
+}
